42604f6f86403e5e91ca1076614488d475297ed9
JT11-11 <168735749+JT11-11@users.noreply.github.com>
2024-11-22 00:37:16 +0800

    First commit

--- CS193P/ContentView.swift
+++ CS193P/ContentView.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        HStack(content:{
+        CardView(isFaceUp: true)
+        CardView(isFaceUp: false)
+        CardView(isFaceUp: false)
+        })
+    }
+}
+
+struct CardView:View{
+    var isFaceUp:Bool=false
+    var body:some View{
+        ZStack(content:{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 11).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 11).strokeBorder(lineWidth: 8)
+                Text("✅").font(.largeTitle)
+            }else{
+                RoundedRectangle(cornerRadius: 11).foregroundColor(.black)
+                RoundedRectangle(cornerRadius: 11).strokeBorder(lineWidth: 8)
+                Text("✅").font(.largeTitle)
+            }
+        })
+        .foregroundColor(.orange)
+        .imageScale(.small)
         .padding()
     }
 }
