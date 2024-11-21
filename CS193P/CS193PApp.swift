cec9f72ed1c73e3a740062fde48c968fef64f2e3
JT11-11 <168735749+JT11-11@users.noreply.github.com>
2024-11-21 20:49:31 +0800

    Initial Commit

---
+++ CS193P/CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193PApp.swift
+//  CS193P
+//
+//  Created by  JT on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
