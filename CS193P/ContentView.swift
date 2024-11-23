46caa79818a373b129ae0a006348cc56e65ef1a9
JT11-11 <168735749+JT11-11@users.noreply.github.com>
2024-11-23 21:10:20 +0800

    Lecture 2.

--- CS193P/ContentView.swift
+++ CS193P/ContentView.swift
@@ -8,41 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis :Array<String>=["🇬🇸","🏳️","🇨🇦","🇨🇦","🇵🇹","🏳️","🇨🇦","🇨🇦","🇵🇹"]
+    
+    @State var cardCount:Int=4
+    
+    
     var body: some View {
-        HStack(content:{
-        CardView(isFaceUp: true)
-        CardView(isFaceUp: false)
-        CardView(isFaceUp: false)
+        VStack{
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCounter
+        }
+        .padding()
+    }
+    
+    var cardCounter: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+            }
+            .imageScale(.large)
+            .font(.largeTitle)
+        }
+    
+    func CardCountAdj(by offset:Int, symbol:String)-> some View{
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName:symbol)
         })
-        
+        .disabled(cardCount+offset<1 || cardCount+offset>emojis.count)
+    }
+    
+    
+    
+    var cardRemover:some View{
+        CardCountAdj(by: -1, symbol: "square.and.arrow.up.on.square")
+    }
+    
+    var cardAdder:some View{
+        CardCountAdj(by: +1, symbol: "square.and.arrow.down.fill")
+    }
+    
+    var cards:some View{
+        LazyVGrid(columns:[GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content:emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.blue)
     }
 }
 
 struct CardView:View{
-    @State var isFaceUp:Bool=false
+    
+    @State var isFaceUp:Bool=true
+    let content:String
     
     var body:some View{
-        ZStack(content:{
+        ZStack{
             let base=Circle()
-            if isFaceUp{
+            Group{
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 8)
-                Text("✅").font(.largeTitle)
-            }else{
-                base.fill(.black)
-                base.strokeBorder(lineWidth: 8)
-                Text("✅").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }
-        })
-        .foregroundColor(.orange)
-        .imageScale(.small)
-        .padding()
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
         .onTapGesture{
-            isFaceUp = !isFaceUp
+            isFaceUp.toggle()
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
